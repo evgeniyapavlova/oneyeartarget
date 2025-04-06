cfba8aeeaf261458a54664bae36779017016a80b
Evgeniya Pavlova <evgeniya.pavlova@quadcode.com>
2025-04-06 18:03:00 +0300

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+	import '$lib/styles/styles.scss';
+</script>
+
+<div class="app">
+	<slot />
+</div>
+<footer>
+	<section>
+		Copyright © Unger Academy® - All right reserved <br />
+
+		Unger Academy S.r.l. - Via Castellana 86/C, 30030 Martellago (VE), Italy - VAT N°: IT04662580275
+		<br />
+
+		<a href="#" target="_blank">Privacy Policy</a> - <a href="#" target="_blank">Terms of Use</a> -
+		<a href="#" target="_blank">Cookie Policy</a> - support@ungeracademy.com
+	</section>
+</footer>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+		padding: 40px 0;
+	}
+</style>
